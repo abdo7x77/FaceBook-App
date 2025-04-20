bed387ae8cc8574423d109e7976dfccf5f5847a9
Abdo <125623049+abdo7x77@users.noreply.github.com>
2025-04-20 06:53:15 +0300

    Update Login.js
    update

--- app/component/Login.js
+++ app/component/Login.js
@@ -6,7 +6,7 @@ export default function Login() {
   return (
     <div className='grid place-items-center '>
         <image
-         src="https://imgs.search.brave.com/_ZXVeCXXBsGPbJeTBgVpe59RA75vprfGnhu7cP81NoM/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9mcmVl/cG5nbG9nby5jb20v/aW1hZ2VzL2FsbF9p/bWcvZmFjZWJvb2st/bG9nby5wbmc"
+         src="https://scontent.fcai19-5.fna.fbcdn.net/v/t39.30808-6/471665472_970124491628782_3667810226357143319_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=D52BHm_oI6IQ7kNvwH1t5sn&_nc_oc=Adn8TCqRxltCZtH1jGL7-DM3TNABKdPKO_tOp26OME0_xj2dAVrc2dAZi37JHguz4IA&_nc_zt=23&_nc_ht=scontent.fcai19-5.fna&_nc_gid=dL8EaadAeqTTWex7Z4YRnQ&oh=00_AfFL1k0dTcqufbBpxENGz3FsN70tPFLeqqFb4dy_cMB0cA&oe=680A534D"
          alt="Facebook Logo"
          width={400}
          height={400}
